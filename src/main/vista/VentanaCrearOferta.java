32868ca5a8e6c5cf1ebf92d816f6b9195cc73744
Odal Sigel <cornagopablo@gmail.com>
2023-06-15 09:54:40 +0200

    Nueva ventana Crear Oferta

---
+++ src/main/vista/VentanaCrearOferta.java
@@ -0,0 +1,105 @@
+package main.vista;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import main.modelo.Empresa;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class VentanaCrearOferta extends JDialog {
+	private final JPanel contentPanel = new JPanel();
+	private Empresa empresa;
+	private JTextField campoPuestoOfertado;
+	private JTextField campoLocalidad;
+	private JTextField campoDescripcion;
+	
+	/**
+	 * Create the dialog.
+	 */
+	public VentanaCrearOferta(Empresa empresa) {
+		this.empresa = empresa;
+		initialize();
+		setDefaultCloseOperation(JDialog.HIDE_ON_CLOSE);
+	}
+
+	/**
+	 * Initialize the dialog
+	 */
+	public void initialize() {
+		setModalityType(DEFAULT_MODALITY_TYPE);
+		setSize(400, 340);
+		setResizable(false);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Puesto ofertado");
+		lblNewLabel.setBounds(10, 11, 100, 14);
+		contentPanel.add(lblNewLabel);
+		
+		campoPuestoOfertado = new JTextField();
+		campoPuestoOfertado.setBounds(10, 36, 364, 20);
+		contentPanel.add(campoPuestoOfertado);
+		campoPuestoOfertado.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Localidad");
+		lblNewLabel_1.setBounds(10, 67, 46, 14);
+		contentPanel.add(lblNewLabel_1);
+		
+		campoLocalidad = new JTextField();
+		campoLocalidad.setBounds(10, 92, 364, 20);
+		contentPanel.add(campoLocalidad);
+		campoLocalidad.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Descripción");
+		lblNewLabel_2.setBounds(10, 123, 100, 14);
+		contentPanel.add(lblNewLabel_2);
+		
+		campoDescripcion = new JTextField();
+		campoDescripcion.setBounds(10, 148, 364, 100);
+		contentPanel.add(campoDescripcion);
+		campoDescripcion.setColumns(10);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if (campoPuestoOfertado.getText().equals("") || campoLocalidad.getText().equals("") || campoDescripcion.getText().equals("")) {
+							JOptionPane.showMessageDialog(getContentPane(), "Por favor, rellene todos los campos");
+						} else {							
+							empresa.crearOferta(campoPuestoOfertado.getText(), campoLocalidad.getText(), campoDescripcion.getText());
+							JOptionPane.showMessageDialog(getContentPane(), "Oferta creada");
+							setVisible(false);
+						}
+					}
+				});
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				buttonPane.add(cancelButton);
+			}
+		}		
+	}
+}
--- src/main/vista/VentanaEmpresa.java
+++ src/main/vista/VentanaEmpresa.java
@@ -2,6 +2,8 @@ package main.vista;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -10,6 +12,9 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableCellRenderer;
 
 import main.modelo.Empresa;
 import main.modelo.ModeloTablaOfertas;
@@ -18,6 +23,7 @@ public class VentanaEmpresa extends JFrame {
 	private JDialog ventanaInicioSesion;
 	private Empresa empresa;
 	private JTable tabla;
+	private VentanaCrearOferta ventanaNuevaOferta;
 
 	/**
 	 * Create the frame.
@@ -25,6 +31,7 @@ public class VentanaEmpresa extends JFrame {
 	public VentanaEmpresa(JDialog ventanaInicioSesion, Empresa empresa) {
 		this.ventanaInicioSesion = ventanaInicioSesion;
 		this.empresa = empresa;
+		ventanaNuevaOferta = new VentanaCrearOferta(empresa);
 		initialize();
 	}
 
@@ -46,21 +53,38 @@ public class VentanaEmpresa extends JFrame {
 		
 		/* Nueva Oferta */
 		JButton btnNuevaOferta = new JButton("Nueva");
+		btnNuevaOferta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ventanaNuevaOferta.setLocationRelativeTo(getContentPane());
+				ventanaNuevaOferta.setVisible(true);
+			}
+		});
 		panelEdicion.add(btnNuevaOferta);
 		
 		/** Ofertas **/		
 		// PROTOTIPO - Rellenar las ofertas del usuario
 		empresa.crearOferta("Analista/Programador", "Soria", "Se busca Analista/Programador en Soria para una importante empresa del sector tecnológico");
+		empresa.crearOferta("Recursos Humanos", "Soria", "Se busca responsable de Recursos Humanos en Soria para una importante empresa del sector tecnológico");
 		
 		/* Tabla */
 		ModeloTablaOfertas modelo = new ModeloTablaOfertas(empresa.getListaOfertas());
+		modelo.addTableModelListener(new TableModelListener() {
+			public void tableChanged(TableModelEvent e) {				
+			}
+		});
 		tabla = new JTable(modelo);
 		tabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		tabla.getTableHeader().setReorderingAllowed(false); // No permitir modificar la posición de las columnas
 		tabla.getTableHeader().setResizingAllowed(false); // No permitir modificar el ancho de las columnas
 		tabla.getColumnModel().getColumn(0).setPreferredWidth(this.getWidth() / 4 * 2); // Ancho de la primera columna -> puesto
 		tabla.getColumnModel().getColumn(1).setPreferredWidth(this.getWidth() / 4); // Ancho de la segunda columna -> fecha
+		DefaultTableCellRenderer alinearCentro = new DefaultTableCellRenderer();
+		alinearCentro.setHorizontalAlignment(DefaultTableCellRenderer.CENTER);
+		tabla.getColumnModel().getColumn(1).setCellRenderer(alinearCentro); // Alinear el texto al centro -> fecha
 		tabla.getColumnModel().getColumn(2).setPreferredWidth(this.getWidth() / 4); // Ancho de la tercera columna -> inscritos
+		DefaultTableCellRenderer alinearDerecha = new DefaultTableCellRenderer();
+		alinearDerecha.setHorizontalAlignment(DefaultTableCellRenderer.RIGHT);
+		tabla.getColumnModel().getColumn(2).setCellRenderer(alinearDerecha); // Alinear el texto a la derecha -> inscritos
 		
 		/* Panel */
 		JScrollPane panelTabla = new JScrollPane(tabla);
