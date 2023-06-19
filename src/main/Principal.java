0c699cae1e9ffeb542cd75addf268fbc85d35af4
Odal Sigel <cornagopablo@gmail.com>
2023-06-19 12:41:58 +0200

    Libreria y fichero de configuracion DB

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -9,6 +9,44 @@ public class Principal {
 		// Cargar Look and Feel personalizado
 		EasyCV.setup();
 
+		// DEBUG
+		// System.out.println("DEBUG");
+		/*
+		// PRUEBA CONEXION BASE DE DATOS
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mariadb://cornagopablo.com:3306/easycv", "easycv", "Projecto1.");
+			System.out.println("OK");
+		} catch (SQLException e) {
+			System.out.println("NO");
+		}
+		*/
+		// PRUEBA DE LECTURA DE FICHERO
+		/*
+		try {
+			FileReader fichero = new FileReader("./resources/db.conf");
+			BufferedReader buf = new BufferedReader(fichero);
+			for (int i=0; i<3; i++) {
+				String textoS = buf.readLine();
+				String texto[] = textoS.split("=");
+				if (texto[0].equals("url")) {
+					System.out.println("URL - " + texto[1]);
+				} else if (texto[0].equals("user")) {
+					System.out.println("USER - " + texto[1]);
+				} else if (texto[0].equals("pass")) {
+					System.out.println("PASSWORD - " + texto[1]);
+				} else {
+					System.out.println("ERROR");
+				}
+			}
+			buf.close();
+			fichero.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("El fichero no existe");
+		} catch (IOException ex) {
+			System.out.println("Erro de lectura");
+		}
+		*/
+		
 		// Crear Ventana de Inicio de sesión
 		try {
 			InicioSesionVentana ventana = new InicioSesionVentana();
