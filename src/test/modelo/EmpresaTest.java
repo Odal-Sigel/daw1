a64e00b153b38d6b675cbdd1854b90dd0108ed77
Odal Sigel <cornagopablo@gmail.com>
2023-06-20 12:08:20 +0200

    Cambio en test

--- src/test/modelo/EmpresaTest.java
+++ src/test/modelo/EmpresaTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import main.modelo.Empresa;
+import main.modelo.excepciones.nifNoValidoException;
 
 class EmpresaTest {
 
@@ -16,13 +17,19 @@ class EmpresaTest {
 		String nif = "56232585T";
 
 		// Objeto
-		Empresa empresa = new Empresa(nickName, password, nombre, nif);
+		Empresa empresa;
+		try {
+			empresa = new Empresa(nickName, password, nombre, nif);
 
-		// Condiciones test
-		assertEquals(nickName, empresa.getNickName());
-		assertEquals(password, empresa.getContraseña());
-		assertEquals(nombre, empresa.getNombre());
-		assertEquals(nif, empresa.getNIF());
+			// Condiciones test
+			assertEquals(nickName, empresa.getNickName());
+			assertEquals(password, empresa.getContraseña());
+			assertEquals(nombre, empresa.getNombre());
+			assertEquals(nif, empresa.getNIF());
+		} catch (nifNoValidoException ex) {
+			fail(ex.getMessage());
+		}
+
 	}
 
 }
